79b0da411cd87bf5e85ada2202070484cd4820b5
Martin Hansson <mhansson@ddn.com>
2025-03-31 11:50:54 +0200

    Moved test files into test/.

---
+++ test/test_parser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+bad_sql=bad.sql
+good_sql=good.sql
+
+# Test the bad SQL
+while read line
+do
+    echo "${line}" | ../build/src/visenten 2>&1 | grep -v "^Error!$"
+    if [ $? == 0 ]
+    then
+        echo "This passed and it shouldn't: $line"
+        exit 1
+    fi
+done < ${bad_sql}
+
+# Test the good SQL
+while read line
+do
+  if [ "${line}" != "" ]
+  then
+    echo "${line}" | ../build/src/visenten 2>&1 | grep -v "^Error!$"
+    if [ $? != 0 ]
+    then
+      echo "This didn't pass and it should: $line"
+      exit 1
+    fi
+  fi
+done < ${good_sql}
